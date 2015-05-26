49d4c07261be802b65a4f50969dd0a3ba0a8ec16
Gilles DOFFE <g.doffe@gmail.com>
2015-05-26 14:15:02 +0200

    Workaround to keep parent relation in epicboard
    Parent relation when creating a story was destroyed at the ruby object creation.
    Updating the story freshly created with same params preserve asked relation.
    
    TODO : Find a better way to do this, it is very quick and dirty fix

--- app/models/rb_story.rb
+++ app/models/rb_story.rb
@@ -158,7 +158,7 @@ class RbStory < Issue
     attribs = Hash[*attribs.flatten]
     s = RbStory.new(attribs)
     s.save!
-    s.position!(params)
+    s.update_and_position!(params)
 
     return s
   end
